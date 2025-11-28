91c3de9873b9610804bc3d051a5878f29bef5f29
Kairose8 <hafizhdikara@gmail.com>
2025-11-29 01:22:03 +0700

    Commit No. 3 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -6,20 +6,32 @@ import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
     init {
         fetchPopularMovies()
     }
     // define the LiveData
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
-    val error: LiveData<String>
-        get() = movieRepository.error
-    // fetch movies from the API
+    private val _popularMovies = MutableStateFlow(
+        emptyList<Movie>()
+    )
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
     private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies()
+            movieRepository.fetchMovies().catch {
+                // catch is a terminal operator that catches exceptions from the Flow
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect {
+                // collect is a terminal operator that collects the values from the Flow
+                // the results are emitted to the StateFlow
+                _popularMovies.value = it
+            }
         }
     }
 }
